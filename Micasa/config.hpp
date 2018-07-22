09dd97bb60b6548992a097b8ba494a17da8e387d
terrakuh <greybley@yahoo.de>
2018-07-22 12:11:28 +0200

    Add play, next and prev resources

--- Micasa/config.hpp
+++ Micasa/config.hpp
@@ -3,4 +3,7 @@
 #define RESOURCE_CLOSE_BUTTON ":/close_button.png"
 #define RESOURCE_INVALID_IMAGE ":/invalid.jpg"
 #define RESOURCE_ICON ":/icon.png"
+#define RESOURCE_PLAY ":/play.png"
+#define RESOURCE_NEXT ":/next.png"
+#define RESOURCE_PREV ":/prev.png"
 #define ZORDER_CLOSE_BUTTON 2
